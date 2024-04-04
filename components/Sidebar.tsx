725766739a6a7a4edb235a9de5c09cf7130c7bdf
Ɓʘɓƴ <shboby71@gmail.com>
2024-04-04 23:19:32 +0530

    6%

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,103 +7,44 @@ export const Sidebar = () => {
             height={100}
             width={100}
             alt="logo"
-            src="https://tse3.mm.bing.net/th?id=OIP.yL98CHypeT0JQEKEVwf_rAHaHa&pid=Api&P=0&h=180"
+            src="https://avatars.githubusercontent.com/u/73479034?v=4"
           />
         </figure>
-        <div className="info-content">
+        <div className="text-white-2 text-fs-3 font-medium tracking-wider leading-normal mb-4">
           <h1 className="name" title="Sharmaji">
-            BOBY
+            BOBY sire
           </h1>
-          <p className="title">Full</p>
+          <p className="title">Full st dev</p>
         </div>
-        <button className="info_more-btn" data-sidebar-btn>
-          <span>Show Contacts</span>
+        <button
+          className="absolute top-0 right-0 rounded-br-lg text-xs text-orange-yellow-crayola bg-border-gradient-onyx p-2 shadow-shadow-2 transition-all z-10"
+          data-sidebar-btn
+        >
+          <span>Contacts</span>
           {/* <ion-icon name="chevron-down"></ion-icon> */}
         </button>
       </div>
 
-      <div className="sidebar-info_more">
+      <div className="opacity-100 visible">
         <div className="size-full h-px bg-gradient-to-r from-indigo-500 my-4 "></div>
-        <ul className="contacts-list">
-          <li className="contact-item">
-            <div className="icon-box">
+        <ul className="grid grid-cols-2 gap-x-8 gap-y-4">
+          <li className="min-w-full flex items-center gap-4">
+            <div className="relative bg-border-gradient-onyx w-30 h-30 rounded-md flex items-center justify-center text-16 text-orange-yellow-crayola shadow-shadow-1 z-10">
               {/* <ion-icon name="mail-outline"></ion-icon> */}
             </div>
-            <div className="contact-info">
-              <p className="contact-title">Email</p>
+            <div className="max-w-calc-100-w-46 w-calc-100-w-46">
+              <p className="text-light-gray-70 text-fs-8 uppercase mb-1">
+                Email Address
+              </p>
               <a href="mailto:shboby7@gmail.com" className="contact-link">
                 shboby7@gmail.com
               </a>
             </div>
           </li>
-          <li className="contact-item">
-            <div className="icon-box">
+          <li className="min-w-full flex items-center gap-4">
+            <div className="relative bg-border-gradient-onyx w-30 h-30 rounded-lg flex items-center justify-center text-16 text-orange-yellow-crayola shadow-shadow-1 z-10">
               {/* <ion-icon name="phone-portrait-outline"></ion-icon> */}
             </div>
-            <div className="contact-info">
-              <p className="contact-title">Phone</p>
-              <a href="tel:+91 74" className="contact-link">
-                +91 74
-              </a>
-            </div>
-          </li>
-          <li className="contact-item">
-            <div className="icon-box">
-              {/* <ion-icon name="calendar-outline"></ion-icon> */}
-            </div>
-            <div className="contact-info">
-              <p className="contact-title">Birthday</p>
-              {/* <time datetime="1982-06-23">April 26, 2003</time> */}
-            </div>
-          </li>
-          <li className="contact-item">
-            <div className="icon-box">
-              {/* <ion-icon name="location-outline"></ion-icon> */}
-            </div>
-            <div className="contact-info">
-              <p className="contact-title">Location</p>
-              {/* <address href="https://www.google.com/maps?q=28.5440653,77.3013517&z=17&hl=en">New Delhi, India</address> */}
-            </div>
-          </li>
-        </ul>
-        <div className="separator"></div>
-        <ul className="social-list">
-          <li className="social-item">
-            <a
-              href="https://www.facebook.com/adil.?mibextid=ZbWKwL"
-              className="social-link"
-            >
-              {/* <ion-icon name="logo-facebook"></ion-icon> */}
-            </a>
-          </li>
-          <li className="social-item">
-            <a
-              href="https://twitter.com/sharmajisenpai?t=DsYHRYBesHTex8hClrkB4g&s=09"
-              className="social-link"
-            >
-              {/* <ion-icon name="logo-twitter"></ion-icon> */}
-            </a>
-          </li>
-          <li className="social-item">
-            <a href="#" className="social-link">
-              {/* <ion-icon name="logo-instagram"></ion-icon> */}
-            </a>
-          </li>
-          <li className="social-item">
-            <a
-              href="https://github.com/helloflixofficial?tab=repositories"
-              className="social-link"
-            >
-              {/* <ion-icon name="logo-github"></ion-icon> */}
-            </a>
-          </li>
-          <li className="social-item">
-            <a
-              href="https://www.linkedin.com/in/helloflixofficial/"
-              className="social-link"
-            >
-              {/* <ion-icon name="logo-linkedin"></ion-icon> */}
-            </a>
           </li>
         </ul>
       </div>
