3ebf931112a02fa82ebd7199fc099780ad422271
Ɓʘɓƴ <shboby71@gmail.com>
2024-04-22 22:38:58 +0530

    11.4%

--- constants/Index.ts
+++ constants/Index.ts
@@ -1,5 +1,44 @@
+import { SidebarLink } from "@/types/Index";
 export const themes = [
-  { value: "light", label: "light", icons: "/public/assets/next.svg" },
-  { value: "dark", label: "dark", icons: "" },
-  { value: "system", label: "system", icons: "" },
+  { value: "light", label: "light", icons: "/assets/443.jpg" },
+  { value: "dark", label: "dark", icons: "/assets/next.svg" },
+  { value: "system", label: "system", icons: "/assets/next.svg" },
+];
+
+export const sidebarLinks: SidebarLink[] = [
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/icons/users.svg",
+    route: "/community",
+    label: "Community",
+  },
+  {
+    imgURL: "/assets/icons/star.svg",
+    route: "/collection",
+    label: "Collections",
+  },
+  {
+    imgURL: "/assets/icons/suitcase.svg",
+    route: "/jobs",
+    label: "Find Jobs",
+  },
+  {
+    imgURL: "/assets/icons/tag.svg",
+    route: "/tags",
+    label: "Tags",
+  },
+  {
+    imgURL: "/assets/icons/user.svg",
+    route: "/profile",
+    label: "Profile",
+  },
+  {
+    imgURL: "/assets/icons/question.svg",
+    route: "/ask-question",
+    label: "Ask a question",
+  },
 ];
